e1b5aa96e7423ccc203e96628655f487efcf8e00
Stefano Lia <liastefano24@gmail.com>
2017-03-23 18:29:38 +0100

    Aggiunta MarkdownBubble

--- client/bubble/MarkdownBubble.js
+++ client/bubble/MarkdownBubble.js
@@ -1,4 +1,30 @@
 /**
- * Created by liast on 23/03/2017
- * Version {VERSION} - {VERSION_NOTES}
+ * Created by Stefano Lia on 23/03/2017
+ * Version 1.0.0 - 1.0.0
  */
+
+class MarkdownBubble extends BaseBubble{
+    /**
+     * @type {Object}
+     */
+    _textView;
+
+    /**
+     * @constructor
+     * Constructor of MarkdownBubble
+     */
+    constructor(){
+        this._textView = new TextWidgetView();
+    }
+
+    /**
+     * @method
+     * Set the text for a MarkdownBubble.
+     * @param (String): represents the string which has to be set in the Widget.
+     * @return
+     */
+    setText(text){
+        this._textView.setText(text);
+    }
+}
+
