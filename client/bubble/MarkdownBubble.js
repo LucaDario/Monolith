fe5a83c775751fd922bf2a6a782e57d3ea6867eb
Stefano Lia <liastefano24@gmail.com>
2017-03-30 15:17:46 +0200

    MArkdownBubble commentata

--- client/bubble/MarkdownBubble.js
+++ client/bubble/MarkdownBubble.js
@@ -17,16 +17,18 @@ export class MarkdownBubble extends BaseBubble{
      */
     constructor(text){
         super();
+        //textView is widget widget of formatted text
         this._textView = new TextWidget();
         this._textView.setFormatText(true);
         this.setText(text);
+        //add the widget in the Bubble's Layout
         this.addComponent(this._textView);
     }
 
     /**
      * @method
      * Set the text for a MarkdownBubble.
-     * @param (String): represents the string which has to be set in the Widget.
+     * @param {String}: represents the string which has to be set in the Widget.
      */
     setText(text){
         this._textView.setText(text);
