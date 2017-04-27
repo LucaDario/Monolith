194fba228e3dd45e3ef7511e331418cd51935479
Stefano Lia <liastefano24@gmail.com>
2017-04-27 17:43:16 +0200

    Sistemati i test di unità

---
+++ client/component/widget/button/presenter/ButtonWidgetPresenter.test.js
@@ -0,0 +1,147 @@
+/**
+ * Created by Diego Cavestro on 27/04/2017
+ * Version 1.0.2 - Lack the TU20, TU21, TU22 and TU23
+ */
+
+import {ButtonWidget} from '../view/ButtonWidget';
+import {ButtonWidgetPresenter} from './ButtonWidgetPresenter';
+import {Exception} from '../../../../exception/Exception';
+
+describe('ButtonWidgetPresenter', function () {
+    it('Check that is instantiable', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode
+        expect(
+            () => {
+                new ButtonWidgetPresenter(new ButtonWidget()); //NOSONAR
+            }
+        ).to.not.throw();
+    });
+
+    it('Check that text is set correctly [TU14]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode
+        expect(
+            () => {
+                const button = new ButtonWidget();
+                button._presenter.renderView();
+                button._presenter.setText("test");
+
+                if (button._presenter._dom.innerHTML !== "test") {
+                    throw new Exception("error");
+                }
+            }
+        ).to.not.throw();
+    });
+
+    it('Check that text is not set not correctly [TU15]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode
+        expect(
+            () => {
+                const button = new ButtonWidget();
+                button._presenter.renderView();
+                button._presenter.setText(123);
+            }
+        ).to.throw();
+    });
+
+    it('Check that dimensions are set correctly [TU16]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        expect(
+            () => {
+                const button = new ButtonWidget();
+                button._presenter.renderView();
+                button._presenter.setWidth("500px");
+                button._presenter.setHeight("600px");
+
+                if (button._presenter._dom.style.width !== "500px" || button._presenter._dom.style.height !== "600px") {
+                    throw new Exception("error");
+                }
+            }
+        ).to.not.throw();
+    });
+
+    it('Check that default dimensions are set correctly [TU17]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        expect(
+            () => {
+                const button = new ButtonWidget();
+                button._presenter.renderView();
+
+                if (button._presenter._dom.style.width === "" || button._presenter._dom.style.height === "") {
+                    throw new Exception("error");
+                }
+            }
+        ).to.not.throw();
+    });
+
+    it('Check that background color is set correctly [TU18]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        expect(
+            () => {
+                const button = new ButtonWidget();
+                button._presenter.renderView();
+
+                button._presenter.setBackgroundColor("#ffffff");
+
+                //this function transforms an rgb value to the correspondent hex value
+                function rgb2hex(rgb) {
+                    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+                    function hex(x) {
+                        return ("0" + parseInt(x).toString(16)).slice(-2);
+                    }
+                    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+                }
+
+                const hex= rgb2hex(button._presenter._dom.style.backgroundColor);
+
+                if (hex !== "#ffffff") {
+                    throw new Exception("error");
+                }
+            }
+        ).to.not.throw();
+    });
+
+    it('Check that default background color is set correctly [TU19]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        expect(
+            () => {
+                const button = new ButtonWidget();
+                button._presenter.renderView();
+
+                if (button._presenter._dom.style.backgroundColor === "") {
+                    throw new Exception("error");
+                }
+            }
+        ).to.not.throw();
+    });
+
+    it('[TU20]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        throw new TypeError("NOT IMPLEMENTED");
+    });
+
+    it('[TU21]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        throw new TypeError("NOT IMPLEMENTED");
+    });
+
+    it('[TU22]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        throw new TypeError("NOT IMPLEMENTED");
+    });
+
+    it('[TU23]', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode - part of TU15
+        throw new TypeError("NOT IMPLEMENTED");
+    });
+});
---
+++ client/component/widget/text/presenter/TextWidgetPresenter.test.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Manuel Turetta on 27/04/2017
+ * Version 1.0.2 - Completed
+ */
+
+import {TextWidgetPresenter} from './TextWidgetPresenter.js';
+import {TextWidget} from '../view/TextWidget';
+
+describe('TextWidgetPresenter', function () {
+    it('Check that is instantiable', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode
+        expect(
+            () => {
+                new TextWidgetPresenter(new TextWidget()); //NOSONAR
+            }
+        ).to.not.throw();
+    });
+
+    it('Check that the font size is correct [TU1]', function () {
+        const tWidget = new TextWidget();
+        tWidget._presenter.setText("CIAO");
+        tWidget._presenter.setTextSize(10);
+        const domNode = tWidget._presenter.renderView();
+        expect(domNode.getElementsByTagName('p')[0].style.fontSize).to.be.eq('10px');
+    });
+
+    it('Check that the default font size is 1em [TU2]', function () {
+        const tWidget = new TextWidget();
+        tWidget._presenter.setText("CIAO");
+        const domNode = tWidget._presenter.renderView();
+        expect(domNode.getElementsByTagName('p')[0].style.fontSize).to.be.eq('1em');
+    });
+
+    it('Check that the italic font style is setted up correctly [TU3]', function () {
+        const tWidget = new TextWidget();
+        tWidget._presenter.setText("*CIAO*");
+        tWidget._presenter.setFormatText(true);
+        const domNode = tWidget._presenter.renderView();
+        expect(domNode.getElementsByTagName('em')[0].innerText).to.be.eq('CIAO');
+    });
+
+    it('Check that the clickable link is setted up correctly [TU4]', function () {
+        const tWidget = new TextWidget();
+        tWidget._presenter.setText("[Google](https://www.google.com)");
+        tWidget._presenter.setFormatText(true);
+        const domNode = tWidget._presenter.renderView().getElementsByTagName('a')[0];
+        expect(domNode.innerText).to.be.eq('Google');
+        expect(domNode.getAttribute('href')).to.be.eq('https://www.google.com');
+    });
+
+    it('Check that the clickable link have the correct color [TU5]', function () {
+        const tWidget = new TextWidget();
+        tWidget._presenter.setText("[Google](https://www.google.com)");
+        tWidget._presenter.setFormatText(true);
+        tWidget._presenter.setUrlHighlightColor('#f00');
+        const domNode = tWidget._presenter.renderView().getElementsByTagName('a')[0];
+        expect(domNode.style.color).to.be.eq('rgb(255, 0, 0)');
+    });
+
+    it('Check that the strong font style is setted up correctly [TU6]', function () {
+        const tWidget = new TextWidget();
+        tWidget._presenter.setText("**Strong** not strong **2Strong**");
+        tWidget._presenter.setFormatText(true);
+        const strongNodes = tWidget._presenter.renderView().getElementsByTagName('strong');
+        expect(strongNodes.length).to.be.eq(2);
+        expect(strongNodes[0].innerText).to.be.eq('Strong');
+        expect(strongNodes[1].innerText).to.be.eq('2Strong');
+
+    });
+
+    it('Check that the default text color is black [TU7]', function () {
+        const tWidget = new TextWidget();
+        expect(tWidget._presenter._textstyle.getColor()).to.be.eq('#000');
+    });
+
+});
